4dc1bcf130416ab78995f5e655df0928681177e8
ilpadre <ilpadre1953@gmail.com>
2019-05-03 15:25:02 -0400

    Add team class

---
+++ sportz/sportz/Models/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sportz.Models
+{
+    public class Team
+    {
+        public string Division { get; set; }
+        public string TeamName { get; set; }
+
+        public Team(string division, string name)
+        {
+            Division = division;
+            TeamName = name;
+        }
+    }
+}
