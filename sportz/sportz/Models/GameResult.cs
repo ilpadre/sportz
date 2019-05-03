f3e77a56572482b372cd379bc050355485f1b015
andycanterbury <andrew.canterbury@ingagepartners.com>
2019-05-03 15:06:03 -0400

    Game result class

---
+++ sportz/sportz/Models/GameResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sportz.Models
+{
+  public class GameResult
+  {
+    DateTime Date { get; set; }
+    string HomeTeam { get; set; }
+    int HomeScore { get; set; }
+    string VisitingTeam { get; set; }
+    int VisitingScore { get; set; }
+
+  }
+}
